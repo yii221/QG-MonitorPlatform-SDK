6b0683e5585954a072f892e98e29c932b65b0022
yii221 <3168707230@qq.com>
2025-08-04 10:57:05 +0800

    feat: 初始化项目

---
+++ pmpSDK/src/test/java/com/pmpsdk/PmpSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmpsdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmpSdkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
